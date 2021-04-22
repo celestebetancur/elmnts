bb60326d19d333ef9937e79174898168a328ddcc
essteban <essteb@gmail.com>
2021-04-22 16:11:07 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import config from './config'
 import { FirebaseAppProvider } from 'reactfire'
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL
+});
 
 ReactDOM.render(
   <React.StrictMode>
-  <FirebaseAppProvider firebaseConfig={config}>
-    <App />
-  </FirebaseAppProvider>
+    <FirebaseAppProvider firebaseConfig={config}>
+      <App/>
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
